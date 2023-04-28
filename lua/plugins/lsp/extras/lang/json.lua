cf5c874eaac789001a66254d158e7b8da04812f0
James Amo <jarmex@gmail.com>
2023-04-28 07:13:18 -0400

    Fix yaml and json configuration (#2)

--- lua/plugins/lsp/extras/lang/json.lua
+++ lua/plugins/lsp/extras/lang/json.lua
@@ -9,13 +9,9 @@ return {
       -- make sure mason installs the server
       servers = {
         jsonls = {
-          -- lazy-load schemastore when needed
-          on_new_config = function(new_config)
-            new_config.settings.json.schemas = new_config.settings.json.schemas or {}
-            vim.list_extend(new_config.settings.json.schemas, require("schemastore").json.schemas())
-          end,
           settings = {
             json = {
+              schemas = require("schemastore").json.schemas(),
               format = {
                 enable = true,
               },
