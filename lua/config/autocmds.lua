8c6d1571201281859ee6c9b2cd9817f313673bd4
James Amo <james.amo@pixovr.com>
2023-05-09 10:45:52 -0400

    Added more autocommands

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -76,3 +76,37 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
     ]])
   end,
 })
+
+-- close some filetypes with <q>
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = {
+    "dap-float",
+  },
+  callback = function(event)
+    vim.bo[event.buf].buflisted = false
+    vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
+  end,
+})
+
+-- close some filetypes with <q>
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = {
+    "dap-terminal",
+  },
+  callback = function(event)
+    vim.keymap.set("n", "q", "<cmd>bdelete!<cr>", { buffer = event.buf, silent = true })
+  end,
+})
+
+-- Fixes Autocomment
+vim.api.nvim_create_autocmd("BufEnter", {
+  pattern = "*",
+  command = "set fo-=c fo-=r fo-=o",
+})
+
+-- resize splits if window got resized
+vim.api.nvim_create_autocmd({ "VimResized" }, {
+  callback = function()
+    vim.cmd("tabdo wincmd =")
+  end,
+})
