a7c2cec6b85cddaedfadbfff6b791fc6eafe0320
James Amo <jamesamo@Jarmex-MacBook.local>
2023-09-09 04:27:14 -0400

    Adding more config to golang

--- lua/plugins/lsp/extras/lang/go.lua
+++ lua/plugins/lsp/extras/lang/go.lua
@@ -23,28 +23,72 @@ return {
           cmd = { "gopls" },
           filetypes = { "go", "gomod", "gowork", "gotmpl" },
           root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+          keys = {
+            -- Workaround for the lack of a DAP strategy in neotest-go: https://github.com/nvim-neotest/neotest-go/issues/12
+            { "<leader>td", "<cmd>lua require('dap-go').debug_test()<CR>", desc = "Debug Nearest (Go)" },
+          },
           settings = {
             gopls = {
-              usePlaceholders = true,
-              completeUnimported = true,
               experimentalPostfixCompletions = true,
               gofumpt = true,
               codelenses = {
+                gc_details = false,
                 generate = true,
-                gc_details = true,
+                regenerate_cgo = true,
+                run_govulncheck = true,
                 test = true,
                 tidy = true,
+                upgrade_dependency = true,
+                vendor = true,
+              },
+              hints = {
+                assignVariableTypes = true,
+                compositeLiteralFields = true,
+                compositeLiteralTypes = true,
+                constantValues = true,
+                functionTypeParameters = true,
+                parameterNames = true,
+                rangeVariableTypes = true,
               },
               analyses = {
+                fieldalignment = true,
+                nilness = true,
                 unusedparams = true,
+                unusedwrite = true,
+                useany = true,
               },
               staticcheck = true,
+              usePlaceholders = true,
+              completeUnimported = true,
+              directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules" },
+              semanticTokens = true,
             },
           },
         }
       },
       -- configure gopls and attach to golang ft
-      setup = {}
+      setup = {
+        gopls = function(_, opts)
+          -- workaround for gopls not supporting semanticTokensProvider
+          -- https://github.com/golang/go/issues/54531#issuecomment-1464982242
+          require("helper").on_lsp_attach(function(client, _)
+            if client.name == "gopls" then
+              if not client.server_capabilities.semanticTokensProvider then
+                local semantic = client.config.capabilities.textDocument.semanticTokens
+                client.server_capabilities.semanticTokensProvider = {
+                  full = true,
+                  legend = {
+                    tokenTypes = semantic.tokenTypes,
+                    tokenModifiers = semantic.tokenModifiers,
+                  },
+                  range = true,
+                }
+              end
+            end
+          end)
+          -- end workaround
+        end,
+      }
     }
   },
 
