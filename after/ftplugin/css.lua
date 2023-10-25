fbf801a218463cab7264cc9c6775bb1b7293005d
James Amo <jamesamo@Jarmex-MacBook.local>
2023-10-25 18:30:28 -0400

    ftplugin css

---
+++ after/ftplugin/css.lua
@@ -0,0 +1,13 @@
+-- toggle !important
+vim.keymap.set("n", "<leader>i", function()
+  local lineContent = vim.api.nvim_get_current_line()
+  if lineContent:find("!important") then
+    lineContent = lineContent:gsub(" !important", "")
+  else
+    lineContent = lineContent:gsub(";?$", " !important;", 1)
+  end
+  vim.api.nvim_set_current_line(lineContent)
+end, { buffer = true, desc = " Toggle !important", nowait = true })
+
+-- SHIMMERING FOCUS SPECIFIC
+if vim.fn.expand("%:t") == "source.css" then require("funcs.shimmering-focus") end
