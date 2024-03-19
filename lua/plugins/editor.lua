91694d2ed7c110ec9130893c98c8c0cee0b4485a
James Amo <james.amo@pixovr.com>
2024-03-19 12:48:26 -0400

    chore(illumiate): change config

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -71,12 +71,7 @@ return {
     "RRethy/vim-illuminate",
     event = { "BufReadPost", "BufNewFile", "BufWritePre" },
     opts = {
-      delay = 300,
-      providers = {
-        'lsp',
-        'treesitter',
-        'regex',
-      },
+      delay = 200,
       large_file_cutoff = 2000,
       large_file_overrides = {
         providers = { "lsp" },
@@ -84,11 +79,39 @@ return {
     },
     config = function(_, opts)
       require("illuminate").configure(opts)
+      local function map(key, dir, buffer)
+        vim.keymap.set("n", key, function()
+          require("illuminate")["goto_" .. dir .. "_reference"](false)
+        end, { desc = dir:sub(1, 1):upper() .. dir:sub(2) .. " Reference", buffer = buffer })
+      end
+
+      map("]]", "next")
+      map("[[", "prev")
+
+      -- also set it after loading ftplugins, since a lot overwrite [[ and ]]
+      vim.api.nvim_create_autocmd("FileType", {
+        callback = function()
+          local buffer = vim.api.nvim_get_current_buf()
+          map("]]", "next", buffer)
+          map("[[", "prev", buffer)
+        end,
+      })
     end,
+    keys = {
+      { "]]", desc = "Next Reference" },
+      { "[[", desc = "Prev Reference" },
+    },
+  },
+
+  -- search/replace in multiple files
+  {
+    "nvim-pack/nvim-spectre",
+    build = false,
+    cmd = "Spectre",
+    opts = { open_cmd = "noswapfile vnew" },
     -- stylua: ignore
     keys = {
-      { "]]", function() require("illuminate").goto_next_reference(false) end, desc = "Next Reference", },
-      { "[[", function() require("illuminate").goto_prev_reference(false) end, desc = "Prev Reference" },
+      { "<leader>sr", function() require("spectre").open() end, desc = "Replace in files (Spectre)" },
     },
   },
 
