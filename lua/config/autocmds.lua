efdc8e1e1a28e9ba9bfc463010f3dee818af8545
James Amo <james.amo@pixovr.com>
2024-03-20 09:23:50 -0400

    feat: disable diagnostics for .env file

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -174,3 +174,12 @@ vim.api.nvim_create_user_command("PixoTest", function(ctx)
   task:start()
   overseer.toggle()
 end, { desc = "Run Pixo Test" })
+
+local group = vim.api.nvim_create_augroup("__env", { clear = true })
+vim.api.nvim_create_autocmd("BufEnter", {
+  pattern = ".env",
+  group = group,
+  callback = function(args)
+    vim.diagnostic.disable(args.buf)
+  end
+})
