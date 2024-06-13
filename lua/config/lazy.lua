629d615e89f1752762b8017bcf6f7894ef9f432e
James Amo <james.amo@pixovr.com>
2024-06-13 13:48:30 -0400

    chore: refactor lsp folder structure

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -12,67 +12,64 @@ end
 
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup(
-  {
-    { import = "plugins" },
-    { import = "plugins.lsp" },
-    { import = "plugins.lsp.lspconfig.server-config" },
+require("lazy").setup({
+  { import = "plugins" },
+  { import = "plugins.lsp" },
+  { import = "plugins.lsp.server-config" },
+}, {
+  defaults = { lazy = false },
+  install = { colorscheme = { "catppuccin", "habamax" } },
+  checker = {
+    enabled = true, -- automatically check for plugin updates
+    notify = false, -- done on my own to use minimum condition for less noise
+    frequency = 60 * 60 * 24, -- = 1 day
   },
-  {
-    defaults = { lazy = false },
-    install = { colorscheme = { "catppuccin", "habamax" } },
-    checker = {
-      enabled = true,           -- automatically check for plugin updates
-      notify = false,           -- done on my own to use minimum condition for less noise
-      frequency = 60 * 60 * 24, -- = 1 day
-    },
-    change_detection = {
-      notify = false
-    },
-    ui = {
-      border = "rounded",
-    },
-    dev = {
-      path = "~/Projects/lua",
-      patterns = { "jarmex" },
-    },
-    diff = {
-      cmd = "diffview.nvim",
-    },
-    readme = { enabled = true },
-    performance = {
-      rtp = {
-        -- disable unused builtin plugins from neovim
-        disabled_plugins = {
-          "man",
-          "matchparen",
-          "matchit",
-          "netrw",
-          "netrwPlugin",
-          "gzip",
-          "zip",
-          "tar",
-          "tarPlugin",
-          "tutor",
-          "rplugin",
-          "health",
-          "tohtml",
-          "zipPlugin",
-        },
+  change_detection = {
+    notify = false,
+  },
+  ui = {
+    border = "rounded",
+  },
+  dev = {
+    path = "~/Projects/lua",
+    patterns = { "jarmex" },
+  },
+  diff = {
+    cmd = "diffview.nvim",
+  },
+  readme = { enabled = true },
+  performance = {
+    rtp = {
+      -- disable unused builtin plugins from neovim
+      disabled_plugins = {
+        "man",
+        "matchparen",
+        "matchit",
+        "netrw",
+        "netrwPlugin",
+        "gzip",
+        "zip",
+        "tar",
+        "tarPlugin",
+        "tutor",
+        "rplugin",
+        "health",
+        "tohtml",
+        "zipPlugin",
       },
     },
-  }
-)
+  },
+})
 
 -- 5s after startup, notify if there many plugin updates
 vim.defer_fn(function()
-  if not require("lazy.status").has_updates() then return end
+  if not require("lazy.status").has_updates() then
+    return
+  end
   local threshold = 15
   local numberOfUpdates = tonumber(require("lazy.status").updates():match("%d+"))
-  if numberOfUpdates < threshold then return end
-  vim.notify(
-    ("󱧕 %s plugin updates"):format(numberOfUpdates),
-    vim.log.levels.INFO,
-    { title = "Lazy" }
-  )
+  if numberOfUpdates < threshold then
+    return
+  end
+  vim.notify(("󱧕 %s plugin updates"):format(numberOfUpdates), vim.log.levels.INFO, { title = "Lazy" })
 end, 5000)
