97e247c1c6a796adf41adf068302215af0a5cb16
James Amo <jamesamo@Jarmex-MacBook.local>
2024-06-20 22:07:57 -0400

    chore(luals): move config

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -68,26 +68,4 @@ return {
       auto_close_after = 3000,
     },
   },
-  {
-    "folke/lazydev.nvim",
-    ft = "lua", -- only load on lua files
-    opts = {
-      library = {
-        "lazy.nvim",
-        -- Load luvit types when the `vim.uv` word is found
-        { path = "luvit-meta/library", words = { "vim%.uv" } },
-      },
-    },
-  },
-  { "Bilal2453/luvit-meta", lazy = true },
-  { -- optional completion source for require statements and module annotations
-    "hrsh7th/nvim-cmp",
-    opts = function(_, opts)
-      opts.sources = opts.sources or {}
-      table.insert(opts.sources, {
-        name = "lazydev",
-        group_index = 0, -- set group index to 0 to skip loading LuaLS completions
-      })
-    end,
-  },
 }
