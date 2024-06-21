8ec3d87248f8105f1dc5aef2ae046e26b7283e5e
James Amo <jamesamo@Jarmex-MacBook.local>
2024-06-20 20:55:31 -0400

    fix(gitsigns): remove depreciated config

--- lua/plugins/tools.lua
+++ lua/plugins/tools.lua
@@ -120,31 +120,18 @@ return {
     },
     opts = {
       signs = {
-        add = { hl = "GitSignsAdd", text = "▎", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-        change = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-        delete = { hl = "GitSignsDelete", text = "契", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-        topdelete = { hl = "GitSignsDelete", text = "契", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-        changedelete = {
-          hl = "GitSignsChange",
-          text = "▎",
-          numhl = "GitSignsChangeNr",
-          linehl = "GitSignsChangeLn",
-        },
+        add = { text = "▎" },
+        change = { text = "▎" },
+        delete = { text = "契" },
+        topdelete = { text = "契" },
+        changedelete = { text = "▎" },
       },
-      numhl = false,
-      linehl = false,
       watch_gitdir = {
         interval = 1000,
         follow_files = true,
       },
       diff_opts = { internal = true },
-      current_line_blame_opts = {
-        virt_text = true,
-        virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
-        delay = 500,
-        ignore_whitespace = false,
-        virt_text_priority = 100,
-      },
+      current_line_blame_opts = { delay = 500 },
       current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <abbrev_sha> - <summary>",
     },
   },
