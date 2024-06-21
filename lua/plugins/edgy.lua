4e6842585b6e4e5e9e7196a89254481b1e684596
James Amo <jamesamo@Jarmex-MacBook.local>
2024-06-20 20:39:54 -0400

    chore: enable edgy

--- lua/plugins/edgy.lua
+++ lua/plugins/edgy.lua
@@ -1,6 +1,6 @@
 return {
   "folke/edgy.nvim",
-  enabled = false,
+  enabled = true,
   event = "VeryLazy",
   init = function()
     vim.opt.laststatus = 3
@@ -8,14 +8,14 @@ return {
   end,
   opts = {
     bottom = {
-      -- {
-      --   ft = "toggleterm",
-      --   size = { height = 0.4 },
-      --   -- exclude floating windows
-      --   filter = function(buf, win)
-      --     return vim.api.nvim_win_get_config(win).relative == ""
-      --   end,
-      -- },
+      {
+        ft = "toggleterm",
+        size = { height = 0.4 },
+        -- exclude floating windows
+        filter = function(_, win)
+          return vim.api.nvim_win_get_config(win).relative == ""
+        end,
+      },
       { ft = "qf", title = "QuickFix" },
       {
         ft = "help",
