33d5231d05d904f6b1f389ea93b858ebe840f28e
James Amo <jamesamo@Jarmex-MacBook.local>
2024-06-27 00:28:41 -0400

    feat: add linting config

--- init.lua
+++ init.lua
@@ -1,4 +1,6 @@
 require("config")
 
 -- CONFIG
-vim.g.borderStyle = "rounded" ---@type "single"|"double"|"rounded"|"solid"|"none"
+vim.g.borderStyle = "rounded" ---@type "single"|"double"|"rounded"|"solid"
+
+vim.g.linterConfigs = vim.fs.normalize("~/.config/nvim/.linter-configs/")
