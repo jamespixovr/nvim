8d75f336256772a7967dea285ba2823ba223df7c
James Amo <jamesamo@Jarmex-MacBook.local>
2024-07-07 21:29:48 -0400

    fix: dap icons

--- lua/plugins/better-escape.lua
+++ lua/plugins/better-escape.lua
@@ -2,17 +2,6 @@ return {
   {
     "max397574/better-escape.nvim",
     event = "InsertEnter",
-    config = function()
-      require("better_escape").setup({
-        mappaings = {
-          i = {
-            j = {
-              k = "<Esc>",
-              j = "<Esc>",
-            },
-          },
-        },
-      })
-    end,
+    config = true,
   },
 }
