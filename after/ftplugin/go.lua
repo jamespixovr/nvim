8c5c78552ee82521cd0103757a8b9302ab41786a
James Amo <jamesamo@Jarmex-MacBook.local>
2024-07-13 00:54:45 -0400

    chore: remove some keymaps

--- after/ftplugin/go.lua
+++ after/ftplugin/go.lua
@@ -1,7 +1,1 @@
-vim.opt_local.commentstring = "//%s"
-vim.opt_local.expandtab = false
-vim.opt_local.shiftwidth = 4
-vim.opt_local.softtabstop = 4
-vim.opt_local.tabstop = 4
-
 vim.keymap.set("n", "<space>td", "<cmd>GoDebug -t<CR>", { buffer = 0 })
