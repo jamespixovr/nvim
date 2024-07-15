43a06b0d13f4ef43a1c86dc817bacabff790aaa1
James Amo <james.amo@pixovr.com>
2024-07-15 09:44:34 -0400

    fix(codecompanion): config

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -37,10 +37,10 @@ return {
       },
       strategies = {
         chat = {
-          adapter = "ollama",
+          adapter = "codeqwen",
         },
         inline = {
-          adapter = "ollama",
+          adapter = "codeqwen",
         },
         agent = {
           adapter = "anthropic",
