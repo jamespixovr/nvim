a2a2cb69aeb6096cc1fdcfd69ee750116924aa17
James Amo <jamesamo@Jarmex-MacBook.local>
2024-07-17 07:54:30 -0400

    fix(keymaps): codeium add transparent background

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,7 +3,14 @@ local M = {
     "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
-    opts = { style = "storm" },
+    opts = {
+      style = "storm",
+      transparent = true,
+      styles = {
+        sidebars = "transparent", -- style for sidebars, see below
+        floats = "transparent", -- style for floating windows
+      },
+    },
   },
   {
     "catppuccin/nvim",
