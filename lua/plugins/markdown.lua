20020513fe64b4ffaf7d819d544f62d5700511db
James Amo <james.amo@pixovr.com>
2024-08-06 09:43:19 -0400

    feat(diagnostics): trying tiny inline

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,14 +1,23 @@
 return {
   {
-    "MeanderingProgrammer/markdown.nvim",
-    enabled = true,
-    name = "render-markdown", -- Only needed if you have another plugin named markdown.nvim
-    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
+    'MeanderingProgrammer/markdown.nvim',
+    enabled = false,
+    name = 'render-markdown', -- Only needed if you have another plugin named markdown.nvim
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
     config = function()
-      require("render-markdown").setup({
+      require('render-markdown').setup({
         latex = { enabled = false },
         acknowledge_conflicts = true,
       })
     end,
+  },
+  {
+    'OXY2DEV/markview.nvim',
+    lazy = false, -- Recommended
+    -- ft = "markdown" -- If you decide to lazy-load anyway
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+      'nvim-tree/nvim-web-devicons',
+    },
   },
 }
