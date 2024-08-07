f93249d69e7fa3c73891bfdec2ddaf9fad557e7f
James Amo <james.amo@pixovr.com>
2024-08-07 09:06:38 -0400

    feat(csv): add csvlens plugin

--- lua/plugins/csv.lua
+++ lua/plugins/csv.lua
@@ -1,7 +1,14 @@
 return {
   {
-    "chrisbra/csv.vim",
-    enabled = false,
-    event = "VeryLazy",
+    'theKnightsOfRohan/csvlens.nvim',
+    dependencies = {
+      'akinsho/toggleterm.nvim',
+    },
+    cmd = { 'Csvlens' },
+    keys = {
+      { '<leader>cv', '<cmd>Csvlens "|"<cr>', desc = "CSV Lens with '|' separator", mode = { 'n', 'v' } },
+    },
+    event = 'VeryLazy',
+    config = true,
   },
 }
