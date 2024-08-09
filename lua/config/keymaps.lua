03c4745db5ca04b2e5a90981f3f874b2a987ba2b
James Amo <james.amo@pixovr.com>
2024-08-09 09:16:40 -0400

    chore: format

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -15,29 +15,29 @@ end
 
 -- Better window movement
 -- Move to window using the <ctrl> hjkl keys
-vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Go to left window", noremap = true })
-vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Go to lower window", noremap = true })
-vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Go to upper window", noremap = true })
-vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Go to right window", noremap = true })
+vim.keymap.set('n', '<C-h>', '<C-w>h', { desc = 'Go to left window', noremap = true })
+vim.keymap.set('n', '<C-j>', '<C-w>j', { desc = 'Go to lower window', noremap = true })
+vim.keymap.set('n', '<C-k>', '<C-w>k', { desc = 'Go to upper window', noremap = true })
+vim.keymap.set('n', '<C-l>', '<C-w>l', { desc = 'Go to right window', noremap = true })
 
 -- local keymap = vim.keymap.set
 
 -- Remap for dealing with word wrap
-keymap("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true })
-keymap("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true })
+keymap('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true })
+keymap('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true })
 
 -- Better viewing
-keymap("n", "n", "nzzzv")
-keymap("n", "N", "Nzzzv")
-keymap("n", "g,", "g,zvzz")
-keymap("n", "g;", "g;zvzz")
+keymap('n', 'n', 'nzzzv')
+keymap('n', 'N', 'Nzzzv')
+keymap('n', 'g,', 'g,zvzz')
+keymap('n', 'g;', 'g;zvzz')
 
 -- Better indent
-keymap("v", "<", "<gv")
-keymap("v", ">", ">gv")
+keymap('v', '<', '<gv')
+keymap('v', '>', '>gv')
 
 -- Paste over currently selected text without yanking it
-keymap("v", "p", '"_dP')
+keymap('v', 'p', '"_dP')
 
 --[[-- jk is mapped to escape by better-escape.nvim plugin]]
 -- 'jk' for quitting insert mode
@@ -45,80 +45,80 @@ keymap("v", "p", '"_dP')
 -- keymap("i", "kj", "<ESC>")
 
 -- COMMAND & INSERT MODE
-keymap({ "i", "c" }, "<C-a>", "<Home>")
-keymap({ "i", "c" }, "<C-e>", "<End>")
+keymap({ 'i', 'c' }, '<C-a>', '<Home>')
+keymap({ 'i', 'c' }, '<C-e>', '<End>')
 
 -- navigation
-keymap("i", "<C-Up>", "<C-\\><C-N><C-w>k")
-keymap("i", "<C-Down>", "<C-\\><C-N><C-w>j")
-keymap("i", "<C-Left>", "<C-\\><C-N><C-w>h")
-keymap("i", "<C-Right>", "<C-\\><C-N><C-w>l")
+keymap('i', '<C-Up>', '<C-\\><C-N><C-w>k')
+keymap('i', '<C-Down>', '<C-\\><C-N><C-w>j')
+keymap('i', '<C-Left>', '<C-\\><C-N><C-w>h')
+keymap('i', '<C-Right>', '<C-\\><C-N><C-w>l')
 
 -- Terminal Mappings
-keymap("t", "<esc><esc>", "<c-\\><c-n>", { desc = "Enter Normal Mode" })
-keymap("t", "<C-h>", "<cmd>wincmd h<cr>", { desc = "Go to left window" })
-keymap("t", "<C-j>", "<cmd>wincmd j<cr>", { desc = "Go to lower window" })
-keymap("t", "<C-k>", "<cmd>wincmd k<cr>", { desc = "Go to upper window" })
-keymap("t", "<C-l>", "<cmd>wincmd l<cr>", { desc = "Go to right window" })
-keymap("t", "<C-/>", "<cmd>close<cr>", { desc = "Hide Terminal" })
-keymap("t", "<c-_>", "<cmd>close<cr>", { desc = "which_key_ignore" })
+keymap('t', '<esc><esc>', '<c-\\><c-n>', { desc = 'Enter Normal Mode' })
+keymap('t', '<C-h>', '<cmd>wincmd h<cr>', { desc = 'Go to left window' })
+keymap('t', '<C-j>', '<cmd>wincmd j<cr>', { desc = 'Go to lower window' })
+keymap('t', '<C-k>', '<cmd>wincmd k<cr>', { desc = 'Go to upper window' })
+keymap('t', '<C-l>', '<cmd>wincmd l<cr>', { desc = 'Go to right window' })
+keymap('t', '<C-/>', '<cmd>close<cr>', { desc = 'Hide Terminal' })
+keymap('t', '<c-_>', '<cmd>close<cr>', { desc = 'which_key_ignore' })
 -- QuickFix
-keymap("n", "]q", ":cnext<CR>")
-keymap("n", "[q", ":cprev<CR>")
-keymap("n", "<C-q>", ":call QuickFixToggle()<CR>")
+keymap('n', ']q', ':cnext<CR>')
+keymap('n', '[q', ':cprev<CR>')
+keymap('n', '<C-q>', ':call QuickFixToggle()<CR>')
 
 -- Clear search with <esc>
-keymap({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>", { desc = "Escape and clear hlsearch" })
-keymap("n", "<leader><space>", ":nohlsearch<CR>")
+keymap({ 'i', 'n' }, '<esc>', '<cmd>noh<cr><esc>', { desc = 'Escape and clear hlsearch' })
+keymap('n', '<leader><space>', ':nohlsearch<CR>')
 
-keymap("n", "<leader>bo", "<cmd>e #<cr>", { desc = "Switch to Other Buffer" })
+keymap('n', '<leader>bo', '<cmd>e #<cr>', { desc = 'Switch to Other Buffer' })
 
 -- save file
 -- keymap({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save file" })
 
 -- lazy
-keymap("n", "<leader>ll", "<cmd>Lazy<cr>", { desc = "Lazy" })
+keymap('n', '<leader>ll', '<cmd>Lazy<cr>', { desc = 'Lazy' })
 
 -- windows
-keymap("n", "<leader>ww", "<C-W>p", { desc = "Other window", remap = true })
-keymap("n", "<leader>wd", "<C-W>c", { desc = "Delete window", remap = true })
-keymap("n", "<leader>w-", "<C-W>s", { desc = "Split window below", remap = true })
-keymap("n", "<leader>w|", "<C-W>v", { desc = "Split window right", remap = true })
-keymap("n", "<leader>-", "<C-W>s", { desc = "Split window below", remap = true })
-keymap("n", "<leader>|", "<C-W>v", { desc = "Split window right", remap = true })
+keymap('n', '<leader>ww', '<C-W>p', { desc = 'Other window', remap = true })
+keymap('n', '<leader>wd', '<C-W>c', { desc = 'Delete window', remap = true })
+keymap('n', '<leader>w-', '<C-W>s', { desc = 'Split window below', remap = true })
+keymap('n', '<leader>w|', '<C-W>v', { desc = 'Split window right', remap = true })
+keymap('n', '<leader>-', '<C-W>s', { desc = 'Split window below', remap = true })
+keymap('n', '<leader>|', '<C-W>v', { desc = 'Split window right', remap = true })
 
-keymap("x", "K", ":m '<-2<CR>gv-gv")
-keymap("x", "J", ":m '>+1<CR>gv-gv")
+keymap('x', 'K', ":m '<-2<CR>gv-gv")
+keymap('x', 'J', ":m '>+1<CR>gv-gv")
 
 -- QUICKFIX
-keymap("n", "gq", vim.cmd.cnext, { desc = " Next Quickfix" })
-keymap("n", "gQ", vim.cmd.cprevious, { desc = " Prev Quickfix" })
-keymap("n", "dQ", function()
-  vim.cmd.cexpr("[]")
-end, { desc = " Delete Quickfix List" })
+keymap('n', 'gq', vim.cmd.cnext, { desc = ' Next Quickfix' })
+keymap('n', 'gQ', vim.cmd.cprevious, { desc = ' Prev Quickfix' })
+keymap('n', 'dQ', function()
+  vim.cmd.cexpr('[]')
+end, { desc = ' Delete Quickfix List' })
 
 -- OPTION TOGGLING
 -- toggle inlay hints
-keymap("n", "<leader>uh", function()
+keymap('n', '<leader>uh', function()
   vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = 0 }), { bufnr = 0 })
 end)
 
 -- Resize windows using <ctrl> arrow keys
-keymap("n", "<C-Up>", ":resize +2<CR>", { desc = "Increase window height", silent = true })
-keymap("n", "<C-Down>", ":resize -2<CR>", { desc = "Decrease window height", silent = true })
-keymap("n", "<C-Left>", ":vertical resize -2<CR>", { desc = "Decrease window width", silent = true })
-keymap("n", "<C-Right>", ":vertical resize +2<CR>", { desc = "Increase window width", silent = true })
+keymap('n', '<C-Up>', ':resize +2<CR>', { desc = 'Increase window height', silent = true })
+keymap('n', '<C-Down>', ':resize -2<CR>', { desc = 'Decrease window height', silent = true })
+keymap('n', '<C-Left>', ':vertical resize -2<CR>', { desc = 'Decrease window width', silent = true })
+keymap('n', '<C-Right>', ':vertical resize +2<CR>', { desc = 'Increase window width', silent = true })
 
-keymap("n", "<leader>cd", vim.diagnostic.open_float, { desc = "Line Diagnostics" })
-keymap("n", "[<tab>", "<cmd>tabprevious<cr>", { desc = "Previous Tab", silent = true })
-keymap("n", "]<tab>", "<cmd>tabnext<cr>", { desc = "Next Tab", silent = true })
+keymap('n', '<leader>cd', vim.diagnostic.open_float, { desc = 'Line Diagnostics' })
+keymap('n', '[<tab>', '<cmd>tabprevious<cr>', { desc = 'Previous Tab', silent = true })
+keymap('n', ']<tab>', '<cmd>tabnext<cr>', { desc = 'Next Tab', silent = true })
 
 M = {}
 
 function M.codeium_keymaps()
   local function local_map(key, func, desc, mode)
     if not mode then
-      mode = "i"
+      mode = 'i'
     end
     return { key, func, mode = mode, expr = true, silent = true, desc = desc }
   end
@@ -135,39 +135,39 @@ end
 
 function M.gen_ai_keymaps()
   return {
-    { "<leader>ia", mode = { "n", "v" }, "<cmd>Gen Ask<cr>", desc = "A[I] [A]sk" },
-    { "<leader>ig", mode = { "n", "v" }, "<cmd>Gen<cr>", desc = "A[I] [G]en" },
-    { "<leader>ic", mode = { "n", "v" }, "<cmd>Gen Change<cr>", desc = "A[I] [C]hange" },
-    { "<leader>io", mode = { "n", "v" }, "<cmd>Gen Change_Code<cr>", desc = "A[I] Change C[o]de" },
-    { "<leader>ih", mode = { "n", "v" }, "<cmd>Gen Chat<cr>", desc = "A[I] C[h]at" },
-    { "<leader>ie", mode = { "n", "v" }, "<cmd>Gen Enhance_Code<cr>", desc = "A[I] [E]nhance code" },
-    { "<leader>iw", mode = { "n", "v" }, "<cmd>Gen Enhance_Wording<cr>", desc = "A[I] Enhance [W]ording" },
-    { "<leader>is", mode = { "n", "v" }, "<cmd>Gen Enhance_Grammar_Spelling<cr>", desc = "A[I] Enhance [G]rammar" },
-    { "<leader>it", mode = { "n", "v" }, "<cmd>Gen Generate<cr>", desc = "A[I] Genera[t]e" },
-    { "<leader>ir", mode = { "n", "v" }, "<cmd>Gen Review_Code<cr>", desc = "A[I] [R]eview Code" },
-    { "<leader>iz", mode = { "n", "v" }, "<cmd>Gen Summarize<cr>", desc = "A[I] Summari[z]e" },
+    { '<leader>ia', mode = { 'n', 'v' }, '<cmd>Gen Ask<cr>', desc = 'A[I] [A]sk' },
+    { '<leader>ig', mode = { 'n', 'v' }, '<cmd>Gen<cr>', desc = 'A[I] [G]en' },
+    { '<leader>ic', mode = { 'n', 'v' }, '<cmd>Gen Change<cr>', desc = 'A[I] [C]hange' },
+    { '<leader>io', mode = { 'n', 'v' }, '<cmd>Gen Change_Code<cr>', desc = 'A[I] Change C[o]de' },
+    { '<leader>ih', mode = { 'n', 'v' }, '<cmd>Gen Chat<cr>', desc = 'A[I] C[h]at' },
+    { '<leader>ie', mode = { 'n', 'v' }, '<cmd>Gen Enhance_Code<cr>', desc = 'A[I] [E]nhance code' },
+    { '<leader>iw', mode = { 'n', 'v' }, '<cmd>Gen Enhance_Wording<cr>', desc = 'A[I] Enhance [W]ording' },
+    { '<leader>is', mode = { 'n', 'v' }, '<cmd>Gen Enhance_Grammar_Spelling<cr>', desc = 'A[I] Enhance [G]rammar' },
+    { '<leader>it', mode = { 'n', 'v' }, '<cmd>Gen Generate<cr>', desc = 'A[I] Genera[t]e' },
+    { '<leader>ir', mode = { 'n', 'v' }, '<cmd>Gen Review_Code<cr>', desc = 'A[I] [R]eview Code' },
+    { '<leader>iz', mode = { 'n', 'v' }, '<cmd>Gen Summarize<cr>', desc = 'A[I] Summari[z]e' },
   }
 end
 
 function M.codecompanion_keymaps()
   return {
-    { "<leader>av", "<cmd>CodeCompanionAdd<cr>", mode = { "v" }, desc = "Add Visual" },
-    { "<leader>ai", "<cmd>CodeCompanion<cr>", mode = { "n", "v" }, desc = "InlineCode" },
-    { "<leader>at", "<cmd>CodeCompanionToggle<CR>", desc = "AI Toggle", mode = { "n", "v" } },
-    { "<leader>ac", "<cmd>CodeCompanionChat<CR>", desc = "AI Chat", mode = { "n", "v" } },
-    { "<leader>aa", "<cmd>CodeCompanionActions<CR>", desc = "[A]I [A]ctions", mode = { "n", "v" } },
+    { '<leader>av', '<cmd>CodeCompanionAdd<cr>', mode = { 'v' }, desc = 'Add Visual' },
+    { '<leader>ai', '<cmd>CodeCompanion<cr>', mode = { 'n', 'v' }, desc = 'InlineCode' },
+    { '<leader>at', '<cmd>CodeCompanionToggle<CR>', desc = 'AI Toggle', mode = { 'n', 'v' } },
+    { '<leader>ac', '<cmd>CodeCompanionChat<CR>', desc = 'AI Chat', mode = { 'n', 'v' } },
+    { '<leader>aa', '<cmd>CodeCompanionActions<CR>', desc = '[A]I [A]ctions', mode = { 'n', 'v' } },
   }
 end
 
 function M.generate_docs_keymaps()
   return {
-    { "<leader>cc", ":Neogen<cr>", "Generate Annotation" },
+    { '<leader>cn', ':Neogen<cr>', 'Generate Annotation' },
   }
 end
 
 function M.symbols_outline_keymaps()
   return {
-    { "<leader>ss", "<cmd>Outline<cr>", desc = "Toggle Outline" },
+    { '<leader>ss', '<cmd>Outline<cr>', desc = 'Toggle Outline' },
   }
 end
 
@@ -205,20 +205,20 @@ end
 function M.dap_ui_keymaps()
   return {
     {
-      "<leader>dI",
+      '<leader>dI',
       function()
-        require("dapui").toggle({})
+        require('dapui').toggle({})
       end,
-      desc = "Dap UI",
+      desc = 'Dap UI',
     },
     {
-      "<leader>de",
+      '<leader>de',
       function() -- Calling this twice to open and jump into the window.
-        require("dapui").eval()
-        require("dapui").eval()
+        require('dapui').eval()
+        require('dapui').eval()
       end,
-      mode = { "n", "v" },
-      desc = "Evaluate expression",
+      mode = { 'n', 'v' },
+      desc = 'Evaluate expression',
     },
   }
 end
@@ -234,47 +234,47 @@ end
 
 function M.terminal_keymaps()
   return {
-    { "<leader>wt", "<cmd>ToggleTerm<cr>", desc = "Toggle Terminal" },
-    { "<leader>wf", "<cmd>ToggleTerm direction=float<cr>", desc = "Toggle Floating Terminal" },
-    { "<leader>wh", "<cmd>ToggleTerm size=10 direction=horizontal<cr>", desc = "Toggle Horizontal Terminal" },
-    { "<leader>wv", "<cmd>ToggleTerm size=80 direction=vertical<cr>", desc = "Toggle Vertical Terminal" },
+    { '<leader>wt', '<cmd>ToggleTerm<cr>', desc = 'Toggle Terminal' },
+    { '<leader>wf', '<cmd>ToggleTerm direction=float<cr>', desc = 'Toggle Floating Terminal' },
+    { '<leader>wh', '<cmd>ToggleTerm size=10 direction=horizontal<cr>', desc = 'Toggle Horizontal Terminal' },
+    { '<leader>wv', '<cmd>ToggleTerm size=80 direction=vertical<cr>', desc = 'Toggle Vertical Terminal' },
   }
 end
 
 function M.gitsigns_keymaps()
   return {
-    { "<leader>gb", "<cmd>Gitsigns toggle_current_line_blame<cr>", desc = "Toggle Git Blame" },
-    { "<leader>gp", "<cmd>Gitsigns preview_hunk<cr>", desc = "Preview Hunk" },
-    { "]g", "<cmd>Gitsigns next_hunk<cr>", desc = "Next Hunk" },
-    { "[g", "<cmd>Gitsigns prev_hunk<cr>", desc = "Prev Hunk" },
-    { "<leader>gs", "<cmd>Gitsigns stage_hunk<cr>", desc = "Stage Hunk" },
-    { "<leader>gu", "<cmd>Gitsigns undo_stage_hunk<cr>", desc = "Undo Stage Hunk" },
-    { "<leader>gr", "<cmd>Gitsigns reset_hunk<cr>", desc = "Reset Hunk" },
-    { "<leader>gR", "<cmd>Gitsigns reset_buffer<cr>", desc = "Reset Buffer" },
+    { '<leader>gb', '<cmd>Gitsigns toggle_current_line_blame<cr>', desc = 'Toggle Git Blame' },
+    { '<leader>gp', '<cmd>Gitsigns preview_hunk<cr>', desc = 'Preview Hunk' },
+    { ']g', '<cmd>Gitsigns next_hunk<cr>', desc = 'Next Hunk' },
+    { '[g', '<cmd>Gitsigns prev_hunk<cr>', desc = 'Prev Hunk' },
+    { '<leader>gs', '<cmd>Gitsigns stage_hunk<cr>', desc = 'Stage Hunk' },
+    { '<leader>gu', '<cmd>Gitsigns undo_stage_hunk<cr>', desc = 'Undo Stage Hunk' },
+    { '<leader>gr', '<cmd>Gitsigns reset_hunk<cr>', desc = 'Reset Hunk' },
+    { '<leader>gR', '<cmd>Gitsigns reset_buffer<cr>', desc = 'Reset Buffer' },
   }
 end
 
 function M.neogit_keymaps()
   return {
-    { "<leader>gn", "<cmd>Neogit<cr>", desc = "Neogit" },
+    { '<leader>gn', '<cmd>Neogit<cr>', desc = 'Neogit' },
   }
 end
 
 function M.setup_coverage_keymaps()
   return {
-    { "<leader>tcc", ":Coverage<CR>", desc = "[t]est [c]overage in gutter" },
-    { "<leader>tcs", ":CoverageLoad<CR>:CoverageSummary<CR>", desc = "[t]est [C]overage summary" },
+    { '<leader>tcc', ':Coverage<CR>', desc = '[t]est [c]overage in gutter' },
+    { '<leader>tcs', ':CoverageLoad<CR>:CoverageSummary<CR>', desc = '[t]est [C]overage summary' },
   }
 end
 
 function M.package_info_keymaps()
   return {
-    { "<leader>pi", "<cmd>lua require('package-info').show()<cr>", desc = "Package Info" },
-    { "<leader>pd", "<cmd>lua require('package-info').delete()<cr>", desc = "Delete Package" },
-    { "<leader>pu", "<cmd>lua require('package-info').update()<cr>", desc = "Update Package" },
-    { "<leader>pU", "<cmd>lua require('package-info').update_all()<cr>", desc = "Update All Packages" },
-    { "<leader>pc", "<cmd>lua require('package-info').change_version()<cr>", desc = "Change Package Version" },
-    { "<leader>pi", "<cmd>lua require('package-info').install()<cr>", desc = "Install Package" },
+    { '<leader>pi', "<cmd>lua require('package-info').show()<cr>", desc = 'Package Info' },
+    { '<leader>pd', "<cmd>lua require('package-info').delete()<cr>", desc = 'Delete Package' },
+    { '<leader>pu', "<cmd>lua require('package-info').update()<cr>", desc = 'Update Package' },
+    { '<leader>pU', "<cmd>lua require('package-info').update_all()<cr>", desc = 'Update All Packages' },
+    { '<leader>pc', "<cmd>lua require('package-info').change_version()<cr>", desc = 'Change Package Version' },
+    { '<leader>pi', "<cmd>lua require('package-info').install()<cr>", desc = 'Install Package' },
   }
 end
 
