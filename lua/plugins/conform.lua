0a0803191ed367a882e70523fc99da225f33c712
James Amo <james.amo@pixovr.com>
2024-08-09 15:09:44 -0400

    chore(conform): disable go

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,17 +1,17 @@
-local linterConfig = vim.fn.stdpath("config") .. "/.linter_configs"
+local linterConfig = vim.fn.stdpath('config') .. '/.linter_configs'
 
 return {
-  "stevearc/conform.nvim",
-  event = { "BufWritePre" },
-  cmd = { "ConformInfo" },
-  dependencies = { "mason.nvim" },
+  'stevearc/conform.nvim',
+  event = { 'BufWritePre' },
+  cmd = { 'ConformInfo' },
+  dependencies = { 'mason.nvim' },
   keys = {
     {
-      "<leader>fd",
+      '<leader>fd',
       function()
-        require("conform").format({ async = true, lsp_fallback = true })
+        require('conform').format({ async = true, lsp_fallback = true })
       end,
-      desc = "Format buffer",
+      desc = 'Format buffer',
     },
   },
   opts = {
@@ -22,33 +22,33 @@ return {
       lsp_fallback = true, -- not recommended to change
     },
     formatters_by_ft = {
-      css = { "prettierd", "prettier", stop_after_first = true },
-      go = { "goimports", "gofumpt" },
-      graphql = { "prettierd", "prettier", stop_after_first = true },
-      handlebars = { "prettier" },
-      html = { "prettierd", "prettier", stop_after_first = true },
-      javascript = { "biome" },
-      javascriptreact = { "biome" },
-      json = { "jq" },
-      lua = { "stylua" },
-      markdown = { "markdownlint", "prettierd", "prettier", "markdown-toc", stop_after_first = true },
-      python = { "isort", "black", stop_after_first = true },
+      css = { 'prettierd', 'prettier', stop_after_first = true },
+      -- go = { "goimports", "gofumpt" },
+      graphql = { 'prettierd', 'prettier', stop_after_first = true },
+      handlebars = { 'prettier' },
+      html = { 'prettierd', 'prettier', stop_after_first = true },
+      javascript = { 'biome' },
+      javascriptreact = { 'biome' },
+      json = { 'jq' },
+      lua = { 'stylua' },
+      markdown = { 'markdownlint', 'prettierd', 'prettier', 'markdown-toc', stop_after_first = true },
+      python = { 'isort', 'black', stop_after_first = true },
       -- sql = { "sql-formatter" },
-      svelte = { "prettierd", "prettier" },
-      typescript = { "biome" },
-      typescriptreact = { "biome" },
-      yaml = { "prettier" },
+      svelte = { 'prettierd', 'prettier' },
+      typescript = { 'biome' },
+      typescriptreact = { 'biome' },
+      yaml = { 'prettier' },
       -- ["*"] = { "trim_whitespace" },
     },
     format_on_save = { timeout_ms = 500, lsp_fallback = true },
     formatters = {
       markdownlint = {
-        command = "markdownlint",
+        command = 'markdownlint',
         stdin = false,
-        args = { "--fix", "--config", linterConfig .. "/markdownlint.yaml", "$FILENAME" },
+        args = { '--fix', '--config', linterConfig .. '/markdownlint.yaml', '$FILENAME' },
       },
       sqlfluff = {
-        args = { "format", "--dialect=ansi", "-" },
+        args = { 'format', '--dialect=ansi', '-' },
       },
     },
   },
