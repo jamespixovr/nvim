4d39d859336c3b45d89d3a614a5c8fa9b40bc3e1
jamespixovr <114949173+jamespixovr@users.noreply.github.com>
2024-08-24 22:47:47 -0400

    feat(overseer): run script

--- lua/plugins/lsp/langs/luals.lua
+++ lua/plugins/lsp/langs/luals.lua
@@ -1,48 +1,48 @@
 return {
   {
-    "neovim/nvim-lspconfig",
+    'neovim/nvim-lspconfig',
     dependencies = {
       {
-        "folke/lazydev.nvim",
-        ft = "lua", -- only load on lua files
+        'folke/lazydev.nvim',
+        ft = 'lua', -- only load on lua files
         opts = {
           library = {
             -- Or relative, which means they will be resolved from the plugin dir.
-            "lazy.nvim",
-            "luvit-meta/library",
-            "neotest",
-            "plenary",
+            'lazy.nvim',
+            'luvit-meta/library',
+            'neotest',
+            'plenary',
             -- Load luvit types when the `vim.uv` word is found
-            { path = "luvit-meta/library", words = { "vim%.uv" } },
+            { path = 'luvit-meta/library', words = { 'vim%.uv' } },
           },
         },
       },
 
-      { "Bilal2453/luvit-meta", lazy = true },
+      { 'Bilal2453/luvit-meta', lazy = true },
 
       { -- optional completion source for require statements and module annotations
-        "hrsh7th/nvim-cmp",
+        'hrsh7th/nvim-cmp',
         opts = function(_, opts)
           opts.sources = opts.sources or {}
           table.insert(opts.sources, {
-            name = "lazydev",
+            name = 'lazydev',
             group_index = 0, -- set group index to 0 to skip loading LuaLS completions
           })
         end,
       },
 
       { -- optional completion source for require statements and module annotations
-        "hrsh7th/nvim-cmp",
+        'hrsh7th/nvim-cmp',
         opts = function(_, opts)
           opts.sources = opts.sources or {}
           table.insert(opts.sources, {
-            name = "lazydev",
+            name = 'lazydev',
             group_index = 0, -- set group index to 0 to skip loading LuaLS completions
           })
         end,
       },
     },
-    ft = { "lua" },
+    ft = { 'lua' },
     opts = {
       -- make sure mason installs the server
       servers = {
@@ -52,57 +52,63 @@ return {
             Lua = {
               runtime = {
                 -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-                version = "LuaJIT",
+                version = 'LuaJIT',
+                special = { reload = 'require' },
               },
               workspace = {
                 checkThirdParty = false,
+                library = {
+                  vim.fn.expand('$VIMRUNTIME/lua'),
+                  vim.fn.expand('$VIMRUNTIME/lua/vim/lsp'),
+                  vim.fn.stdpath('data') .. '/lazy/lazy.nvim/lua/lazy',
+                },
               },
               codeLens = {
                 enable = true,
               },
               completion = {
                 workspaceWord = true,
-                callSnippet = "Both",
+                callSnippet = 'Both',
                 enable = true,
-                showWord = "Disable",
+                showWord = 'Disable',
               },
               diagnostics = {
                 groupSeverity = {
-                  strong = "Warning",
-                  strict = "Warning",
+                  strong = 'Warning',
+                  strict = 'Warning',
                 },
                 groupFileStatus = {
-                  ["ambiguity"] = "Opened",
-                  ["await"] = "Opened",
-                  ["codestyle"] = "None",
-                  ["duplicate"] = "Opened",
-                  ["global"] = "Opened",
-                  ["luadoc"] = "Opened",
-                  ["redefined"] = "Opened",
-                  ["strict"] = "Opened",
-                  ["strong"] = "Opened",
-                  ["type-check"] = "Opened",
-                  ["unbalanced"] = "Opened",
-                  ["unused"] = "Opened",
+                  ['ambiguity'] = 'Opened',
+                  ['await'] = 'Opened',
+                  ['codestyle'] = 'None',
+                  ['duplicate'] = 'Opened',
+                  ['global'] = 'Opened',
+                  ['luadoc'] = 'Opened',
+                  ['redefined'] = 'Opened',
+                  ['strict'] = 'Opened',
+                  ['strong'] = 'Opened',
+                  ['type-check'] = 'Opened',
+                  ['unbalanced'] = 'Opened',
+                  ['unused'] = 'Opened',
                 },
-                unusedLocalExclude = { "_*" },
-                globals = { "vim" },
+                unusedLocalExclude = { '_*' },
+                globals = { 'vim' },
               },
               misc = {
                 parameters = {
-                  "--log-level=trace",
+                  '--log-level=trace',
                 },
               },
               doc = {
-                privateName = { "^_" },
+                privateName = { '^_' },
               },
               hint = {
                 enable = true, -- enabled inlay hints
                 setType = false,
                 paramType = true,
-                paramName = "Disable",
-                semicolon = "Disable",
-                arrayIndex = "Disable",
+                paramName = 'Disable',
+                semicolon = 'Disable',
+                arrayIndex = 'Disable',
               },
             },
           },
