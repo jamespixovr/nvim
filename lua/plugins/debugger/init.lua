b531d963efc453c850204fd9dbd1816082706989
James Amo <james.amo@pixovr.com>
2024-08-28 15:46:02 -0400

    change dapui

--- lua/plugins/debugger/init.lua
+++ lua/plugins/debugger/init.lua
@@ -1,33 +1,33 @@
-local icons = require("settings").icons.dap
+local icons = require('settings').icons.dap
 
 --------------------------------------------------------------------------------------
 
 local function dapConfig()
-  vim.fn.sign_define("DapStopped", { text = icons.Stopped, texthl = "DiagnosticHint", linehl = "DapPause" })
-  vim.fn.sign_define("DapBreakpoint", { text = icons.Breakpoint, texthl = "DiagnosticInfo", linehl = "DapBreak" })
-  vim.fn.sign_define("DapBreakpointRejected", { text = icons.BreakpointRejected, texthl = "DiagnosticError" })
+  vim.fn.sign_define('DapStopped', { text = icons.Stopped, texthl = 'DiagnosticHint', linehl = 'DapPause' })
+  vim.fn.sign_define('DapBreakpoint', { text = icons.Breakpoint, texthl = 'DiagnosticInfo', linehl = 'DapBreak' })
+  vim.fn.sign_define('DapBreakpointRejected', { text = icons.BreakpointRejected, texthl = 'DiagnosticError' })
 
   -- use overseer for running preLaunchTask and postDebugTask
-  require("overseer").enable_dap()
+  require('overseer').enable_dap()
 
-  require("dap.ext.vscode").load_launchjs("launch.json")
+  require('dap.ext.vscode').load_launchjs('launch.json')
 
   -- AUTO-OPEN/CLOSE THE DAP-UI
-  local listener = require("dap").listeners.before
+  local listener = require('dap').listeners.before
   listener.attach.dapui_config = function()
-    require("dapui").open()
+    require('dapui').open()
   end
   listener.launch.dapui_config = function()
-    require("dapui").open()
+    require('dapui').open()
   end
   listener.event_terminated.dapui_config = function()
-    require("dapui").close()
+    require('dapui').close()
   end
   listener.event_exited.dapui_config = function()
-    require("dapui").close()
+    require('dapui').close()
   end
 
-  require("plugins.debugger.typescript")
+  require('plugins.debugger.typescript')
 end
 
 return {
@@ -38,44 +38,34 @@ return {
   --  Debuggers don't have system dependencies, you just install them with mason.
   --  We currently ship most of them with nvim.
   {
-    "mfussenegger/nvim-dap",
-    event = "VeryLazy",
-    keys = require("config.keymaps").dap_keymaps(),
+    'mfussenegger/nvim-dap',
+    event = 'VeryLazy',
+    keys = require('config.keymaps').dap_keymaps(),
 
     dependencies = {
-      { "theHamsta/nvim-dap-virtual-text", opts = { virt_text_pos = "eol" } },
+      { 'theHamsta/nvim-dap-virtual-text', opts = { virt_text_pos = 'eol' } },
     },
 
     config = dapConfig,
   },
 
   { -- fancy UI for the debugger
-    "rcarriga/nvim-dap-ui",
-    event = "VeryLazy",
-    dependencies = { "nvim-neotest/nvim-nio" },
+    'rcarriga/nvim-dap-ui',
+    event = 'VeryLazy',
+    dependencies = { 'nvim-neotest/nvim-nio' },
     -- stylua: ignore
     keys = require("config.keymaps").dap_ui_keymaps(),
     opts = {
-      icons = { expanded = "▾", collapsed = "▸" },
+      icons = { expanded = '▾', collapsed = '▸' },
       -- floating = { border = 'rounded' },
       layouts = {
         {
           elements = {
-            -- { id = 'stacks',      size = 0.30 },
-            -- { id = 'breakpoints', size = 0.20 },
-            { id = "scopes", size = 0.50 },
-            { id = "watches", size = 0.50 },
+            { id = 'repl', size = 0.4 },
+            { id = 'scopes', size = 0.6 },
           },
-          position = "right",
-          size = 40,
-        },
-        {
-          elements = {
-            { id = "repl", size = 0.5 },
-            { id = "console", size = 0.5 },
-          },
-          position = "bottom",
-          size = 10,
+          position = 'bottom',
+          size = 15,
         },
       },
       floating = {
@@ -84,7 +74,7 @@ return {
         -- max_width = 0.9,
         -- border = "rounded",
         mappings = {
-          close = { "q", "<Esc>" },
+          close = { 'q', '<Esc>' },
         },
       },
       render = {
@@ -94,10 +84,10 @@ return {
   },
 
   { -- mason.nvim integration
-    "jay-babu/mason-nvim-dap.nvim",
-    event = "VeryLazy",
-    dependencies = "mason.nvim",
-    cmd = { "DapInstall", "DapUninstall" },
+    'jay-babu/mason-nvim-dap.nvim',
+    event = 'VeryLazy',
+    dependencies = 'mason.nvim',
+    cmd = { 'DapInstall', 'DapUninstall' },
     opts = {
       -- Makes a best effort to setup the various debuggers with
       -- reasonable debug configurations
@@ -114,17 +104,17 @@ return {
   },
   --  Debugging with go debugger
   {
-    "leoluz/nvim-dap-go",
-    event = "VeryLazy",
+    'leoluz/nvim-dap-go',
+    event = 'VeryLazy',
     config = function()
-      require("dap-go").setup({
+      require('dap-go').setup({
         dap_configurations = {
           {
-            type = "go",
-            name = "Debug (Main) Package",
-            request = "launch",
-            program = "main.go",
-            cwd = "${workspaceFolder}",
+            type = 'go',
+            name = 'Debug (Main) Package',
+            request = 'launch',
+            program = 'main.go',
+            cwd = '${workspaceFolder}',
           },
         },
       })
