aaaa6156dc9194004f40ea18687e0d03138f00dd
James Amo <jarmex@gmail.com>
2024-10-07 19:51:02 -0400

    chore(glance): disable glance

--- lua/plugins/lsp/glance.lua
+++ lua/plugins/lsp/glance.lua
@@ -1,9 +1,10 @@
 ---@diagnostic disable: missing-fields
 return {
   {
-    "dnlhc/glance.nvim",
-    event = "VeryLazy",
-    cmd = { "Glance" },
+    'dnlhc/glance.nvim',
+    enabled = false,
+    event = 'VeryLazy',
+    cmd = { 'Glance' },
     opts = {
       use_trouble_qf = true,
       height = 20,
@@ -28,13 +29,13 @@ return {
         end,
       },
       folds = {
-        fold_closed = "",
-        fold_open = "",
+        fold_closed = '',
+        fold_open = '',
         folded = true,
       },
       indent_lines = {
         enable = true,
-        icon = "│",
+        icon = '│',
       },
       winbar = {
         enable = true,
