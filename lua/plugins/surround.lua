283961b3e9d928b37be84f4054bbb7df7ae5675e
James Amo <jarmex@gmail.com>
2024-10-22 22:20:44 -0400

    refactor: noice + surround

--- lua/plugins/surround.lua
+++ lua/plugins/surround.lua
@@ -4,8 +4,39 @@ return {
     version = '*', -- Use for stability; omit to use `main` branch for the latest features
     -- enabled = false,
     event = 'VeryLazy',
+    keys = {
+      'ys',
+      'ds',
+      'cs',
+      { 'S', mode = 'x' },
+      { '<C-g>s', mode = 'i' },
+    },
     config = function()
-      require('nvim-surround').setup({})
+      require('nvim-surround').setup({
+        keymaps = {
+          insert = '<C-g>s',
+          insert_line = '<C-g>S',
+          normal = 'ys',
+          normal_cur = 'yss',
+          normal_line = 'yS',
+          normal_cur_line = 'ySS',
+          visual = 'S',
+          visual_line = 'gS',
+          delete = 'ds',
+          change = 'cs',
+        },
+        aliases = {
+          ['a'] = '<',
+          ['b'] = '(',
+          ['B'] = '{',
+          ['r'] = '[',
+          ['q'] = { '"', "'", '`' },
+          ['s'] = { '{', '[', '(', '<', '"', "'", '`' },
+        },
+        highlight = {
+          duration = 500,
+        },
+      })
     end,
   },
 }
