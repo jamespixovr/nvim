94f2a029f8e38b24189129b793ee38931ed5c348
James Amo <jarmex@gmail.com>
2024-10-22 22:20:12 -0400

    chore(lazygit): disable plugin

--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -1,21 +1,22 @@
 -- nvim v0.8.0
 return {
-  "kdheepak/lazygit.nvim",
-  event = "VeryLazy",
+  'kdheepak/lazygit.nvim',
+  enabled = false,
+  event = 'VeryLazy',
   cmd = {
-    "LazyGit",
-    "LazyGitConfig",
-    "LazyGitCurrentFile",
-    "LazyGitFilter",
-    "LazyGitFilterCurrentFile",
+    'LazyGit',
+    'LazyGitConfig',
+    'LazyGitCurrentFile',
+    'LazyGitFilter',
+    'LazyGitFilterCurrentFile',
   },
   -- optional for floating window border decoration
   dependencies = {
-    "nvim-lua/plenary.nvim",
+    'nvim-lua/plenary.nvim',
   },
   -- setting the keybinding for LazyGit with 'keys' is recommended in
   -- order to load the plugin when the command is run for the first time
   keys = {
-    { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+    { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
   },
 }
