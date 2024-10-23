ef7bb005b462a129758ba57c28a872a23c39545f
James Amo <jarmex@gmail.com>
2024-10-22 22:20:44 -0400

    refactor: noice + surround

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -5,24 +5,24 @@ local routes = {
   {
     filter = {
       min_height = 10,
-      ["not"] = {
+      ['not'] = {
         cond = function(msg)
-          local title = (msg.opts and msg.opts.title) or ""
-          return title:find("Commit Preview") or title:find("tinygit") or title:find("Changed Files")
+          local title = (msg.opts and msg.opts.title) or ''
+          return title:find('Commit Preview') or title:find('tinygit') or title:find('Changed Files')
         end,
       },
     },
-    view = "popup",
+    view = 'popup',
   },
   -- output from `:Inspect`
-  { filter = { event = "msg_show", find = "Treesitter.*- @" }, view = "popup" },
+  { filter = { event = 'msg_show', find = 'Treesitter.*- @' }, view = 'popup' },
   {
     filter = {
       any = {
-        { find = "No active Snippet" },
-        { find = "No signature help available" },
-        { find = "^<$" },
-        { kind = "wmsg" },
+        { find = 'No active Snippet' },
+        { find = 'No signature help available' },
+        { find = '^<$' },
+        { kind = 'wmsg' },
       },
     },
     opts = { skip = true },
@@ -31,88 +31,88 @@ local routes = {
   -- REDIRECT TO MINI
 
   -- write/deletion messages
-  { filter = { event = "msg_show", find = "%d+B written$" }, view = "mini" },
-  { filter = { event = "msg_show", find = "%d+L, %d+B$" }, view = "mini" },
-  { filter = { event = "msg_show", find = "%-%-No lines in buffer%-%-" }, view = "mini" },
+  { filter = { event = 'msg_show', find = '%d+B written$' }, view = 'mini' },
+  { filter = { event = 'msg_show', find = '%d+L, %d+B$' }, view = 'mini' },
+  { filter = { event = 'msg_show', find = '%-%-No lines in buffer%-%-' }, view = 'mini' },
 
   -- search pattern not found
-  { filter = { event = "msg_show", find = "^E486: Pattern not found" }, view = "mini" },
+  { filter = { event = 'msg_show', find = '^E486: Pattern not found' }, view = 'mini' },
 
   -- Word added to spellfile via `zg`
-  { filter = { event = "msg_show", find = "^Word .*%.add$" }, view = "mini" },
+  { filter = { event = 'msg_show', find = '^Word .*%.add$' }, view = 'mini' },
 
   { -- Mason
     filter = {
-      event = "notify",
+      event = 'notify',
       cond = function(msg)
-        return msg.opts and (msg.opts.title or ""):find("mason")
+        return msg.opts and (msg.opts.title or ''):find('mason')
       end,
     },
-    view = "mini",
+    view = 'mini',
   },
 
   -- nvim-treesitter
-  { filter = { event = "msg_show", find = "^%[nvim%-treesitter%]" }, view = "mini" },
-  { filter = { event = "notify", find = "All parsers are up%-to%-date" }, view = "mini" },
+  { filter = { event = 'msg_show', find = '^%[nvim%-treesitter%]' }, view = 'mini' },
+  { filter = { event = 'notify', find = 'All parsers are up%-to%-date' }, view = 'mini' },
 
   -----------------------------------------------------------------------------
   -- SKIP
 
   -- FIX LSP bugs?
-  { filter = { event = "msg_show", find = "lsp_signature? handler RPC" }, skip = true },
+  { filter = { event = 'msg_show', find = 'lsp_signature? handler RPC' }, skip = true },
   {
-    filter = { event = "msg_show", find = "^%s*at process.processTicksAndRejections" },
+    filter = { event = 'msg_show', find = '^%s*at process.processTicksAndRejections' },
     skip = true,
   },
 
   -- code actions
-  { filter = { event = "notify", find = "No code actions available" }, skip = true },
+  { filter = { event = 'notify', find = 'No code actions available' }, skip = true },
 
   -- unneeded info on search patterns
-  { filter = { event = "msg_show", find = "^[/?]." }, skip = true },
+  { filter = { event = 'msg_show', find = '^[/?].' }, skip = true },
 
   -- E211 no longer needed, since auto-closing deleted buffers
-  { filter = { event = "msg_show", find = "E211: File .* no longer available" }, skip = true },
+  { filter = { event = 'msg_show', find = 'E211: File .* no longer available' }, skip = true },
 }
 
 --------------------------------------------------------------------------------
 return {
   {
-    "folke/noice.nvim",
-    event = "VimEnter", -- earlier to catch notifications on startup
+    'folke/noice.nvim',
+    event = 'VimEnter', -- earlier to catch notifications on startup
     enabled = true,
     dependencies = {
-      "MunifTanjim/nui.nvim",
-      "rcarriga/nvim-notify",
+      'MunifTanjim/nui.nvim',
+      'rcarriga/nvim-notify',
     },
     opts = {
       routes = routes,
       cmdline = {
         format = {
-          search_down = { icon = "  " },
-          search_up = { icon = "  " },
-          filter = { icon = "  " },
-          lua = { icon = "  " },
-          cmdline = { pattern = "^:", icon = "|>", lang = "vim", title = "" },
+          search_down = { icon = '  ' },
+          search_up = { icon = '  ' },
+          filter = { icon = '  ' },
+          lua = { icon = '  ' },
+          cmdline = { pattern = '^:', icon = '|>', lang = 'vim', title = '' },
         },
       },
       views = {
         cmdline_popup = {
           position = {
             row = 15,
-            col = "50%",
+            col = '50%',
           },
           size = {
-            width = "75%",
-            height = "auto",
+            width = '75%',
+            height = 'auto',
           },
           border = { style = vim.g.borderStyle },
         },
         popupmenu = {
-          relative = "editor",
+          relative = 'editor',
           position = {
-            row = "50%",
-            col = "50%",
+            row = '50%',
+            col = '50%',
           },
           size = {
             width = 60,
@@ -130,7 +130,7 @@ return {
           timeout = 3000,
           zindex = 4, -- lower, so it does not cover nvim-notify (zindex 50)
           position = { col = -3 }, -- to the left to avoid collision with scrollbar
-          format = { "{title} ", "{message}" }, -- leave out "{level}"
+          format = { '{title} ', '{message}' }, -- leave out "{level}"
         },
         hover = {
           border = { style = vim.g.borderStyle },
@@ -140,14 +140,14 @@ return {
         popup = {
           border = { style = vim.g.borderStyle },
           size = { width = 90, height = 25 },
-          win_options = { scrolloff = 8, wrap = true, concealcursor = "nv" },
-          close = { keys = { "q", "<D-w>", "<D-9>", "<D-0>" } },
+          win_options = { scrolloff = 8, wrap = true, concealcursor = 'nv' },
+          close = { keys = { 'q', '<D-w>', '<D-9>', '<D-0>' } },
         },
         split = {
           enter = true,
-          size = "50%",
+          size = '50%',
           win_options = { scrolloff = 6 },
-          close = { keys = { "q", "<D-w>", "<D-9>", "<D-0>" } },
+          close = { keys = { 'q', '<D-w>', '<D-9>', '<D-0>' } },
         },
       },
       lsp = {
@@ -156,9 +156,9 @@ return {
         hover = { enabled = false },
         -- ENABLE features
         override = {
-          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-          ["vim.lsp.util.stylize_markdown"] = true,
-          ["cmp.entry.get_documentation"] = true,
+          ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+          ['vim.lsp.util.stylize_markdown'] = true,
+          ['cmp.entry.get_documentation'] = true,
         },
       },
       presets = {
@@ -172,74 +172,73 @@ return {
     },
     keys = {
       {
-        "<S-Enter>",
+        '<S-Enter>',
         function()
-          require("noice").redirect(vim.fn.getcmdline())
+          require('noice').redirect(vim.fn.getcmdline())
         end,
-        mode = "c",
-        desc = "Redirect Cmdline",
+        mode = 'c',
+        desc = 'Redirect Cmdline',
       },
       {
-        "<leader>nl",
+        '<leader>nl',
         function()
-          require("noice").cmd("last")
+          require('noice').cmd('last')
         end,
-        desc = "Noice Last Message",
+        desc = 'Noice Last Message',
       },
       {
-        "<leader>nh",
+        '<leader>nh',
         function()
-          require("noice").cmd("history")
+          require('noice').cmd('history')
         end,
-        desc = "Noice History",
+        desc = 'Noice History',
       },
       {
-        "<leader>na",
+        '<leader>na',
         function()
-          require("noice").cmd("all")
+          require('noice').cmd('all')
         end,
-        desc = "Noice All",
+        desc = 'Noice All',
       },
       {
-        "<leader>nd",
+        '<leader>nd',
         function()
-          require("noice").cmd("dismiss")
+          require('noice').cmd('dismiss')
         end,
-        desc = "Dismiss All",
+        desc = 'Dismiss All',
       },
       {
-        "<c-f>",
+        '<c-f>',
         function()
-          if not require("noice.lsp").scroll(4) then
-            return "<c-f>"
+          if not require('noice.lsp').scroll(4) then
+            return '<c-f>'
           end
         end,
         silent = true,
         expr = true,
-        desc = "Scroll forward",
-        mode = { "i", "n", "s" },
+        desc = 'Scroll forward',
+        mode = { 'i', 'n', 's' },
       },
       {
-        "<c-b>",
+        '<c-b>',
         function()
-          if not require("noice.lsp").scroll(-4) then
-            return "<c-b>"
+          if not require('noice.lsp').scroll(-4) then
+            return '<c-b>'
           end
         end,
         silent = true,
         expr = true,
-        desc = "Scroll backward",
-        mode = { "i", "n", "s" },
+        desc = 'Scroll backward',
+        mode = { 'i', 'n', 's' },
       },
     },
   },
   {
-    "rcarriga/nvim-notify",
+    'rcarriga/nvim-notify',
+    event = 'BufReadPre',
     opts = {
       timeout = 3000,
-      background_colour = "#000000",
-      -- stages = "slide",
-      -- level = 0,
+      background_colour = '#000000',
       max_height = function()
         return math.floor(vim.o.lines * 0.75)
       end,
@@ -247,13 +246,17 @@ return {
       max_width = function()
         return math.floor(vim.o.columns * 0.75)
       end,
+      stages = 'fade_in_slide_out',
+      top_down = true,
+      render = 'simple',
+      fps = 120,
       -- Icons for the different levels
       icons = {
-        ERROR = "",
-        WARN = "",
-        INFO = "",
-        DEBUG = "",
-        TRACE = "✎",
+        ERROR = '',
+        WARN = '',
+        INFO = '',
+        DEBUG = '',
+        TRACE = '✎',
       },
     },
   },
