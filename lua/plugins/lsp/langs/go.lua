36ed49af56ff04b98af7fbffc7fb313c8e09603d
James Amo <jarmex@gmail.com>
2024-10-22 22:29:00 -0400

    feat(lsp/langs/go.lua): add auto-fix-return and impl plugins

--- lua/plugins/lsp/langs/go.lua
+++ lua/plugins/lsp/langs/go.lua
@@ -91,6 +91,25 @@ return {
       },
     },
   },
+  {
+    'Jay-Madden/auto-fix-return.nvim',
+    config = function()
+      require('auto-fix-return').setup({})
+    end,
+    lazy = false,
+  },
+  {
+    'jack-rabe/impl.nvim',
+    dependencies = {
+      'nvim-telescope/telescope.nvim',
+    },
+    opts = {
+      layout_strategy = 'vertical',
+      layout_config = {
+        width = 0.5,
+      },
+    },
+  },
 
   {
     'maxandron/goplements.nvim',
