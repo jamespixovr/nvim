1cdbe113cbe1bf383fa785ccb7ead8e41810b328
James Amo <james.amo@pixovr.com>
2024-11-26 10:10:51 -0500

    refactor(codecompanion): remove unused 'telescope.nvim' dependency, add 'fzf_lua' for slash commands
    refactor(markdown): enable render modes and disable sign in status column

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -5,6 +5,10 @@ return {
     ft = { 'markdown', 'codecompanion' },
     opts = {
       latex = { enabled = false },
+      render_modes = true, -- Render in ALL modes
+      sign = {
+        enabled = false, -- Turn off in the status column
+      },
     },
   },
 }
