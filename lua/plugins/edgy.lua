18be64c843029a2db2c0108ebc0719f815db6a78
James Amo <jarmex@gmail.com>
2025-02-14 18:49:02 -0500

    chore: disable edgy

--- lua/plugins/edgy.lua
+++ lua/plugins/edgy.lua
@@ -1,64 +1,66 @@
 return {
-  'folke/edgy.nvim',
-  enabled = true,
-  event = 'VeryLazy',
-  init = function()
-    vim.opt.laststatus = 3
-    vim.opt.splitkeep = 'screen'
-  end,
-  opts = {
-    animate = { enabled = false },
-    bottom = {
-      {
-        ft = 'toggleterm',
-        size = { height = 0.4 },
-        -- exclude floating windows
-        filter = function(_, win)
-          return vim.api.nvim_win_get_config(win).relative == ''
-        end,
-      },
-      {
-        ft = 'snacks_terminal',
-        size = { height = 0.4 },
-        title = '%{b:snacks_terminal.id}: %{b:term_title}',
-        filter = function(_buf, win)
-          return vim.w[win].snacks_win
-            and vim.w[win].snacks_win.position == 'bottom'
-            and vim.w[win].snacks_win.relative == 'editor'
-            and not vim.w[win].trouble_preview
-        end,
-      },
-      { ft = 'qf', title = 'QuickFix' },
-      {
-        ft = 'help',
-        size = { height = 20 },
-        -- only show help buffers
-        filter = function(buf)
-          return vim.bo[buf].buftype == 'help'
-        end,
-      },
-      { ft = 'neotest-output-panel', title = 'Neotest OutputPanel', size = { height = 0.3 } },
-      {
-        ft = 'Trouble',
-        title = 'TROUBLE',
-        filter = function(buf, win)
-          return vim.api.nvim_win_get_config(win).relative == ''
-        end,
-      },
-      {
-        ft = 'noice',
-        size = { height = 0.4 },
-        filter = function(buf, win)
-          return vim.api.nvim_win_get_config(win).relative == ''
-        end,
-      },
-    },
-    left = {
-      { ft = 'neotest-summary', title = 'Neotest Summary', size = { width = 0.4 } },
-    },
-    right = {
-      { ft = 'codecompanion', title = 'Code Companion Chat', size = { width = 0.45 } },
-      -- { ft = "aerial", title = "Symbols", size = { width = 0.2 } },
-    },
-  },
+  -- {
+  --   'folke/edgy.nvim',
+  --   enabled = true,
+  --   event = 'VeryLazy',
+  --   init = function()
+  --     vim.opt.laststatus = 3
+  --     vim.opt.splitkeep = 'screen'
+  --   end,
+  --   opts = {
+  --     animate = { enabled = false },
+  --     bottom = {
+  --       {
+  --         ft = 'toggleterm',
+  --         size = { height = 0.4 },
+  --         -- exclude floating windows
+  --         filter = function(_, win)
+  --           return vim.api.nvim_win_get_config(win).relative == ''
+  --         end,
+  --       },
+  --       {
+  --         ft = 'snacks_terminal',
+  --         size = { height = 0.4 },
+  --         title = '%{b:snacks_terminal.id}: %{b:term_title}',
+  --         filter = function(_buf, win)
+  --           return vim.w[win].snacks_win
+  --             and vim.w[win].snacks_win.position == 'bottom'
+  --             and vim.w[win].snacks_win.relative == 'editor'
+  --             and not vim.w[win].trouble_preview
+  --         end,
+  --       },
+  --       { ft = 'qf', title = 'QuickFix' },
+  --       {
+  --         ft = 'help',
+  --         size = { height = 20 },
+  --         -- only show help buffers
+  --         filter = function(buf)
+  --           return vim.bo[buf].buftype == 'help'
+  --         end,
+  --       },
+  --       { ft = 'neotest-output-panel', title = 'Neotest OutputPanel', size = { height = 0.3 } },
+  --       {
+  --         ft = 'Trouble',
+  --         title = 'TROUBLE',
+  --         filter = function(buf, win)
+  --           return vim.api.nvim_win_get_config(win).relative == ''
+  --         end,
+  --       },
+  --       {
+  --         ft = 'noice',
+  --         size = { height = 0.4 },
+  --         filter = function(buf, win)
+  --           return vim.api.nvim_win_get_config(win).relative == ''
+  --         end,
+  --       },
+  --     },
+  --     left = {
+  --       { ft = 'neotest-summary', title = 'Neotest Summary', size = { width = 0.4 } },
+  --     },
+  --     right = {
+  --       -- { ft = 'codecompanion', title = 'Code Companion Chat', size = { width = 0.45 } },
+  --       -- { ft = "aerial", title = "Symbols", size = { width = 0.2 } },
+  --     },
+  --   },
+  -- },
 }
